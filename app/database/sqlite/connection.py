0bf87332846ce543aa3da50f768e2fb31d219a16
Victorlima1488 <victorlima.softwaredeveloper@gmail.com>
2024-03-27 23:44:38 -0300

    feat: registration route creation

---
+++ app/database/sqlite/connection.py
@@ -0,0 +1,13 @@
+import sqlite3
+from sqlite3 import Error
+
+def conexao_do_banco():
+    db_file = 'app/database/database.db'
+    
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as er:
+        print(er)
+    return conn
+
+vconn = conexao_do_banco()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from  fastapi import FastAPI
+from routers.router_login import auth_router
 import uvicorn
 
 app = FastAPI()
@@ -6,6 +7,8 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {"message": "Hello, world!"}
+
+app.include_router(auth_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
